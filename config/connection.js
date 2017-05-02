51b858bc0582bbd2eb642e5ec206037b4d268dfd
rukaroa <rukaroa@gmail.com>
2017-05-02 18:47:47 -0500

    sequelize added

--- config/connection.js
+++ config/connection.js
@@ -13,14 +13,6 @@ if (process.env.JAWSDB_URL) {
   });
 };
 
-// var connection = mysql.createConnection({
-//   port: 3306,
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "burgers_db"
-// });
-
 // Make connection.
 connection.connect(function(err) {
   if (err) {
