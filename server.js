1f96f3fc9c5e93c956ce88782d20d226e7c6a448
rukaroa <rukaroa@gmail.com>
2017-05-04 18:17:08 -0500

    server updated

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 var express = require("express");
-var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 
 var port = process.env.PORT || 3000;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + "/public"));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
+var db = require("./models");
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -20,9 +13,24 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_Controller.js");
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.use("/", routes);
+// Static directory
+app.use(express.static("./public"));
 
-app.listen(port);
+// Routes =============================================================
+
+require("./routes/html-routes.js")(app);
+require("./routes/post-api-routes.js")(app);
+require("./routes/author-api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our express app
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
