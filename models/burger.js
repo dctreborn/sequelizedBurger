a32120762669133bb1c7a2a797bc92c3fb281026
rukaroa <rukaroa@gmail.com>
2017-05-04 18:43:02 -0500

    model started

--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,30 @@
-var orm = require("../config/orm.js");
+// var orm = require("../config/orm.js");
 
-var burger = {
-	selectAll: function (cb) {
-		orm.selectAll("burgers", function(res){
-			cb(res);
-		});
-	},
-	insertOne: function (cols, vals, cb) {
-		orm.insertOne("burgers", cols, vals, function(res){
-			cb(res);
-		});
-	},
-	updateOne: function (objColsVals, condition, cb) {
-		orm.updateOne("burgers", objColsVals, condition, function(res){
-			cb(res);
-		});
-	}
+// var burger = {
+// 	selectAll: function (cb) {
+// 		orm.selectAll("burgers", function(res){
+// 			cb(res);
+// 		});
+// 	},
+// 	insertOne: function (cols, vals, cb) {
+// 		orm.insertOne("burgers", cols, vals, function(res){
+// 			cb(res);
+// 		});
+// 	},
+// 	updateOne: function (objColsVals, condition, cb) {
+// 		orm.updateOne("burgers", objColsVals, condition, function(res){
+// 			cb(res);
+// 		});
+// 	}
+// };
+
+// module.exports = burger;
+
+module.exports = function(sequelize, DataTypes) {
+	var Burger = sequelize.define("Burger", {
+		burger_name: DataTypes.STRING,
+		devoured: DataTypes.BOOLEAN,
+		
+	})
 };
 
-module.exports = burger;
