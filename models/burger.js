75caab7ff119c926427d612322f318d35603472a
Rukaroa <rukaroa@gmail.com>
2017-05-06 15:44:53 -0500

    model created

--- models/burger.js
+++ models/burger.js
@@ -1,30 +1,18 @@
-// var orm = require("../config/orm.js");
-
-// var burger = {
-// 	selectAll: function (cb) {
-// 		orm.selectAll("burgers", function(res){
-// 			cb(res);
-// 		});
-// 	},
-// 	insertOne: function (cols, vals, cb) {
-// 		orm.insertOne("burgers", cols, vals, function(res){
-// 			cb(res);
-// 		});
-// 	},
-// 	updateOne: function (objColsVals, condition, cb) {
-// 		orm.updateOne("burgers", objColsVals, condition, function(res){
-// 			cb(res);
-// 		});
-// 	}
-// };
-
-// module.exports = burger;
-
 module.exports = function(sequelize, DataTypes) {
 	var Burger = sequelize.define("Burger", {
-		burger_name: DataTypes.STRING,
-		devoured: DataTypes.BOOLEAN,
-		
-	})
+		burger_name: {
+			type: DataTypes.STRING,
+			validate: {
+				allowNull: false,
+				notEmpty: true,
+				len: [1, 100]
+			}
+		},
+		devoured: {
+			type: DataTypes.BOOLEAN,
+			defaultValue: false
+		}		
+	});
+	return Burger;
 };
 
